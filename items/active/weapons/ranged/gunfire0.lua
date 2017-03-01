33dde8ad36e47bee4be0968bc47396ed834790be
IHC <seele.sans.corpus@gmail.com>
2017-03-01 00:02:24 -0500

    throw now disapears

--- items/active/weapons/ranged/gunfire0.lua
+++ items/active/weapons/ranged/gunfire0.lua
@@ -19,21 +19,13 @@ function GunFire:update(dt, fireMode, shiftHeld)
 
   self.cooldownTimer = math.max(0, self.cooldownTimer - self.dt)
 
-  if animator.animationState("firing") ~= "fire" then
-    animator.setLightActive("muzzleFlash", false)
-  end
-
   if self.fireMode == (self.activatingFireMode or self.abilitySlot)
     and not self.weapon.currentAbility
     and self.cooldownTimer == 0
     and not status.resourceLocked("energy")
     and not world.lineTileCollision(mcontroller.position(), self:firePosition()) then
 
-    if self.fireType == "auto" and status.overConsumeResource("energy", self:energyPerShot()) then
-      self:setState(self.auto)
-    elseif self.fireType == "burst" then
-      self:setState(self.burst)
-    end
+    self:fireProjectile()
   end
 end
 
@@ -41,7 +33,6 @@ function GunFire:auto()
   self.weapon:setStance(self.stances.fire)
 
   self:fireProjectile()
-  self:muzzleFlash()
 
   if self.stances.fire.duration then
     util.wait(self.stances.fire.duration)
@@ -57,7 +48,6 @@ function GunFire:burst()
   local shots = self.burstCount
   while shots > 0 and status.overConsumeResource("energy", self:energyPerShot()) do
     self:fireProjectile()
-    self:muzzleFlash()
     shots = shots - 1
 
     self.weapon.relativeWeaponRotation = util.toRadians(interp.linear(1 - shots / self.burstCount, 0, self.stances.fire.weaponRotation))
@@ -70,29 +60,8 @@ function GunFire:burst()
 end
 
 function GunFire:cooldown()
-  self.weapon:setStance(self.stances.cooldown)
-  self.weapon:updateAim()
-
-  local progress = 0
-  util.wait(self.stances.cooldown.duration, function()
-    local from = self.stances.cooldown.weaponOffset or {0,0}
-    local to = self.stances.idle.weaponOffset or {0,0}
-    self.weapon.weaponOffset = {interp.linear(progress, from[1], to[1]), interp.linear(progress, from[2], to[2])}
-
-    self.weapon.relativeWeaponRotation = util.toRadians(interp.linear(progress, self.stances.cooldown.weaponRotation, self.stances.idle.weaponRotation))
-    self.weapon.relativeArmRotation = util.toRadians(interp.linear(progress, self.stances.cooldown.armRotation, self.stances.idle.armRotation))
-
-    progress = math.min(1.0, progress + (self.dt / self.stances.cooldown.duration))
-  end)
-end
-
-function GunFire:muzzleFlash()
-  animator.setPartTag("muzzleFlash", "variant", math.random(1, 3))
-  animator.setAnimationState("firing", "fire")
-  animator.burstParticleEmitter("muzzleFlash")
-  animator.playSound("fire")
-
-  animator.setLightActive("muzzleFlash", true)
+  item.consume(1)
+  world.spawnItem(tp)
 end
 
 function GunFire:fireProjectile(projectileType, projectileParams, inaccuracy, firePosition, projectileCount)
@@ -103,9 +72,6 @@ function GunFire:fireProjectile(projectileType, projectileParams, inaccuracy, fi
 
   if not projectileType then
     projectileType = self.projectileType
-  end
-  if type(projectileType) == "table" then
-    projectileType = projectileType[math.random(#projectileType)]
   end
 
   local projectileId = 0
@@ -123,6 +89,7 @@ function GunFire:fireProjectile(projectileType, projectileParams, inaccuracy, fi
         params
       )
   end
+  self:setState(self.cooldown)
   return projectileId
 end
 
